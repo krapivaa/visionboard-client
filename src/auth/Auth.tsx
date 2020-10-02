5bb227729e6a360e50ebb1193f9341b96aab7273
thecoder-droid <thegabrielfrancis@gmail.com>
2020-10-01 20:21:54 -0400

    message

---
+++ src/auth/Auth.tsx
@@ -0,0 +1,19 @@
+// export interface AuthProps {
+    
+// }
+ 
+// export interface AuthState {
+    
+// }
+ 
+// class Auth extends React.Component<AuthProps, AuthState> {
+//     constructor(props: AuthProps) {
+//         super(props);
+//         this.state = { :  };
+//     }
+//     render() { 
+//         return (  );
+//     }
+// }
+ 
+// export default Auth;
