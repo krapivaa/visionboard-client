e4d6e76e2346e3d7a8853d01f7087b304bb7a92b
Marina Iv <marisha@Marinas-MBP.home>
2020-10-10 10:29:46 -0400

    board display edits

--- src/boardDisplay/BoardDisplay.tsx
+++ src/boardDisplay/BoardDisplay.tsx
@@ -6,15 +6,38 @@ import Container from '@material-ui/core/Container';
 import ItemHomeinBoard from '../itemDisplay/ItemHomeinBoard';
 import { Theme } from '@material-ui/core/styles';
 import { withStyles } from "@material-ui/core/styles";
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+// import tileData from './tileData';
+import BoardCreate from './BoardCreate';
 
 const useStyles = (theme: Theme) => ({
     root: {
-      '& > *': {
-        margin: theme.spacing(1),
+        display: 'flex',
+        // flexWrap: 'wrap', incompatible?
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
       },
-    },
-
-  });
+      gridList: {
+        width: 500,
+        height: 450,
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+      },
+      titleBar: {
+        background:
+          'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+          'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+      },
+      icon: {
+        color: 'white',
+      },
+    })
+  
 
 
 export interface BoardDisplayProps {
@@ -31,21 +54,45 @@ class BoardDisplay extends React.Component<BoardDisplayProps, BoardDisplayState>
         // this.state = { :  };
     }
     render() { 
+      
+        // export default function AdvancedGridList() {
 
         const {classes}: any = this.props;
 
-        return ( <div style={{ backgroundColor: 'white'  }}>
+    return ( <div style={{ backgroundColor: 'white'  }}>
             <h5 >All my boards:</h5> 
 
         <Container maxWidth="sm" style={{ backgroundColor: '#cfe8fc' }} > 
        My board:<br />     
         <ItemHomeinBoard />
-  
         </Container>
+
+{/* 
+        <div className={classes.root}>
+      <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              titlePosition="top"
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+              className={classes.titleBar}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div> */}
 
 
         </div>);
     }
+//   }
 }
  
 export default withStyles(useStyles)(BoardDisplay);
