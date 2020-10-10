5221480c138adb7fcbd7a96b4f893f9fa1e6b107
Lauren Anthony <laurenanthony247@gmail.com>
2020-10-10 14:53:00 -0400

    updated casing on board/item display files, Updated Auth toggle now functioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,40 @@
+import React, { useState } from "react";
+import "./App.css";
+import Auth from "./auth/Auth";
+// import BoardHome from "./boardDisplay/BoardHome";
 
-import React, { useState } from 'react';
-import './App.css';
-import Auth from './auth/Auth';
-// import  Login from './auth/Login';
-// import BoardHome from './boardDisplay/boardHome';
-
-
-<!--   function App() {
-    const [token, setToken] = useState("");
-    return (
-      <div className="image1">
-        <h1>Vision Board App</h1>
-        <Auth setToken={setToken}/>
-         {/* <BoardHome  /> */} -->
-
-
-
-
-
-export interface AppProps {}
-
-export interface AppState {}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    // this.state = { :  };
-  }
-  render() {
-    return (
-      <div>
-
-        <h1 style={{textAlign:"center"}}>Vision Board App</h1>
-        <Navigation />
-        <!--         <Auth setToken={setToken}/>       -->
-        <BoardHome />
-
-
-      </div>
-    );
-  }
+function App() {
+  const [token, setToken] = useState("");
+  return (
+    <div className="image1">
+      <h1>Vision Board App</h1>
+      <Auth setToken={setToken} />
+      {/* <BoardHome  /> */}
+    </div>
+  );
+}
 
 export default App;
+
+// export interface AppProps {}
+
+// export interface AppState {}
+
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
+//     // this.state = { :  };
+//   }
+//   render() {
+//     return (
+//       <div>
+
+//         <h1 style={{textAlign:"center"}}>Vision Board App</h1>
+//         <Navigation />
+//         <!--         <Auth setToken={setToken}/>       -->
+//         <BoardHome /> */}
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/boardDisplay/boardHome.tsx
+++ src/boardDisplay/boardHome.tsx
@@ -1,21 +1,19 @@
 //Example of Worklog Index - responsible for loading other board components. It is like "board landing page"
-import React from 'react';
-import '../App.css';
-import BoardCreate from './BoardCreate';
-import BoardUpdate from './BoardUpdate';
-import BoardDisplay from './BoardDisplay';
-import { Theme } from '@material-ui/core/styles';
+import React from "react";
+import "../App.css";
+import BoardCreate from "./BoardCreate";
+// import BoardUpdate from "./BoardUpdate";
+import BoardDisplay from "./BoardDisplay";
+import { Theme } from "@material-ui/core/styles";
 import { withStyles } from "@material-ui/core/styles";
-import Grid from '@material-ui/core/Grid';
-
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = (theme: Theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
     },
   },
-
 });
 
 /* //TODO:
@@ -23,51 +21,53 @@ const useStyles = (theme: Theme) => ({
 
 */
 
+export interface BoardHomeProps {}
 
-export interface BoardHomeProps {
-   
-}
- 
-export interface BoardHomeState {
-    
-    
-}
+export interface BoardHomeState {}
 
 class BoardHome extends React.Component<BoardHomeProps, BoardHomeState> {
+  heading = "Board Home";
 
-    heading = "Board Home";
+  constructor(props: BoardHomeProps) {
+    super(props);
+    // this.state = {  : [] };
+  }
 
-    constructor(props: BoardHomeProps) {
-        super(props);
-        // this.state = {  : [] };
-    }
-    
-    render() { 
+  render() {
+    const { classes }: any = this.props;
 
-      const {classes}: any = this.props;
+    return (
+      <div>
+        <h2
+          style={{
+            paddingLeft: "50px",
+            color: "darkgrey",
+            textAlign: "center",
+          }}
+        >
+          {this.heading}
+        </h2>
 
-        return ( <div>
-            <h2 style={{ paddingLeft: "50px", color: 'darkgrey', textAlign: "center"}} >{this.heading}</h2>
+        <Grid container spacing={3}>
+          <Grid item xs={3} style={{ backgroundColor: "#cfe8fc" }}>
+            <BoardCreate
+              token={
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTYwMjE4ODcwMywiZXhwIjoxNjAyMjc1MTAzfQ.Y4yEAqaRbsepRjeU8oL2GZIcCc0OSzPn5jI4boK70z4"
+              }
+            />
+          </Grid>
 
-      <Grid container spacing={3}>
-
-      <Grid item xs={3} style={{ backgroundColor: '#cfe8fc' }}>
-        <BoardCreate token={"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTYwMjE4ODcwMywiZXhwIjoxNjAyMjc1MTAzfQ.Y4yEAqaRbsepRjeU8oL2GZIcCc0OSzPn5jI4boK70z4"} />
+          <Grid item xs={9} style={{ backgroundColor: "lightblue" }}>
+            <BoardDisplay />
+          </Grid>
         </Grid>
-      
-        <Grid item xs={9} style={{ backgroundColor: 'lightblue' }}>
-        <BoardDisplay />
-        </Grid>
-       
-      </Grid>
-
-        </div> );
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default withStyles(useStyles)(BoardHome);
 
 /*
 token={"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTYwMjE4ODcwMywiZXhwIjoxNjAyMjc1MTAzfQ.Y4yEAqaRbsepRjeU8oL2GZIcCc0OSzPn5jI4boK70z4"}
 */
-
