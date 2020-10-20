7f7548c5813731e497e0f4f3f58598a1c52efa83
thecoder-droid <thegabrielfrancis@gmail.com>
2020-10-19 20:52:37 -0400

    added component to AdminTable

--- src/admin/EditUser.tsx
+++ src/admin/EditUser.tsx
@@ -1,21 +1,2 @@
 import React from 'react';
 
-// const urlEndpoint = "http://localhost:3000/api/admin/view-all";
-
-//             let adminHeaders = new Headers();
-//             adminHeaders.append("Content-Type", "application/json");
-
-//             const requestOptions = {
-//               method: "DELETE",
-//               headers: adminHeaders,
-//             };
-
-//             const deleteUser = (user) => {
-//             fetch(urlEndpoint, requestOptions)
-//               .then((res: any) => res.json())
-//               .then((json) => {
-//                 this.props.token(json.sessionToken);
-//                 console.log(json);
-//             })
-//         }
-//     }
--- src/home/Navigation.tsx
+++ src/home/Navigation.tsx
@@ -31,7 +31,7 @@ const useStyles = (theme: Theme) => ({
     [theme.breakpoints.up("sm")]: {
       width: drawerWidth,
       flexShrink: 0,
-    },
+    }},
 
     appBar: {
       [theme.breakpoints.up("sm")]: {
